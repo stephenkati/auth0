1724cdd8bc9a4c28cedb7cf06ec4dc85eb9ddc31
stephenkati <stephenkatuli88@gmail.com>
2023-08-31 11:20:02 +0300

    Setup auth0

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Login from './components/Login'
+import Logout from './components/Logout'
+import Profile from './components/Profile'
+
+const App = () => {
+
+  return (
+    <>
+      <Login />
+      <Logout />
+      <Profile />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const domain = import.meta.env.REACT_APP_DOMAIN
+const clientId = import.meta.env.REACT_APP_CLIENT
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
+)
